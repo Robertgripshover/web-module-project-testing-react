a7fd7743ed073e9ead615b160b2264b5256a1e8a
Robert Gripshover <robertgrips@gmail.com>
2023-02-03 13:27:33 -0500

    got the first test suite to work

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -21,6 +21,23 @@ const exampleEpisodeData = {
     url: "https://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
   };
 
+  const exampleEpisodeDataWithoutImage = {
+    airdate: "2016-07-15",
+    airstamp: "2016-07-15T12:00:00+00:00",
+    airtime: "",
+    id: 553946,
+    image: null,
+    name: "Chapter One: The Vanishing of Will Byers",
+    number: 1,
+    rating: { average: 8.2 },
+    runtime: 49,
+    season: 1,
+    summary:
+      "test summary",
+    type: "regular",
+    url: "https://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
+  };
+
 
 test("renders without error", () => {
     render(<Episode episode={exampleEpisodeData}/>)
@@ -34,7 +51,11 @@ test("renders the summary test passed as prop", () => {
     expect(summary).toHaveTextContent("test summary")
 });
 
-test("renders default image when image is not defined", () => {});
+test("renders default image when image is not defined", () => {
+    render(<Episode episode={exampleEpisodeDataWithoutImage}/>)
+    const altImageTest = screen.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png')
+    expect(altImageTest).toBeInTheDocument()
+});
 
 // ----- EXAMPLE EPISODE TEST OBJECT -----
 
